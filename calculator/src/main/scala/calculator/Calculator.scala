32c37043571fd59843ee2e4346107a87f0963b4e
Cameron Burley <cameronkburley@gmail.com>
2023-10-14 21:54:41 -0500

    Calculator Solution

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -1,5 +1,7 @@
 package calculator
 
+import calculator.Expr.Divide
+
 enum Expr:
   case Literal(v: Double)
   case Ref(name: String)
@@ -11,12 +13,23 @@ enum Expr:
 object Calculator extends CalculatorInterface:
  import Expr.*
 
-  def computeValues(
-      namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] =
-    ???
+  def computeValues(namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] =
+    namedExpressions.map { case (name, expr) =>
+      (name, Signal(eval(expr(), namedExpressions)))
+    }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]])(using Signal.Caller): Double =
-    ???
+    expr match {
+      case Literal(v) => v
+      case Plus(a, b) => eval(a, references) + eval(b, references)
+      case Minus(a, b) => eval(a, references) - eval(b, references)
+      case Times(a, b) => eval(a, references) * eval(b, references)
+      case Divide(a, b) =>
+        val denominator = eval(b, references)
+        if (denominator != 0) eval(a, references) / denominator
+        else Double.NaN // Return NaN for division by zero
+      case Ref(name) => eval(getReferenceExpr(name, references), references.filter(_._1 != name))
+    }
 
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
--- calculator/src/main/scala/calculator/Polynomial.scala
+++ calculator/src/main/scala/calculator/Polynomial.scala
@@ -1,10 +1,29 @@
 package calculator
 
-object Polynomial extends PolynomialInterface:
-  def computeDelta(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double]): Signal[Double] =
-    ???
+import scala.math.sqrt
 
-  def computeSolutions(a: Signal[Double], b: Signal[Double],
-      c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] =
-    ???
+
+object Polynomial extends PolynomialInterface:
+  def computeDelta(a: Signal[Double], b: Signal[Double], c: Signal[Double]): Signal[Double] =
+    Signal {
+      val ba = b()
+      val aa = a()
+      val ca = c()
+      ba * ba - 4 * aa * ca
+    }
+
+  def computeSolutions(a: Signal[Double], b: Signal[Double], c: Signal[Double], delta: Signal[Double]): Signal[Set[Double]] =
+    Signal {
+      val deltaVal = delta()
+      val ba = b()
+      val aa = a()
+      if (deltaVal < 0) Set() // No real solutions
+      else if (deltaVal == 0) Set(-ba / (2 * aa)) // One real solution
+      else { // Two real solutions
+        val sqrtDelta = sqrt(deltaVal)
+        Set(
+          (-ba + sqrtDelta) / (2 * aa),
+          (-ba - sqrtDelta) / (2 * aa)
+        )
+      }
+    }
